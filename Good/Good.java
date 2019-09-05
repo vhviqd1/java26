67fe669ec7de19154b25fd46fe784c8d5cd7617e
vhviqd1 <54919838+vhviqd1@users.noreply.github.com>
2019-09-05 09:04:34 +0800

    添加一句话进行 修改，并要求pr
    添加一句话进行 修改，并要求pr

--- Good/Good.java
+++ Good/Good.java
@@ -1,9 +1,11 @@
 class Good
 {
 	public static void main(String[] args){
-		System.out.println("ÄãºÃ");
+		System.out.println("Ã„Ã£ÂºÃƒ");
 
 		System.out.println("good good study,day day up!");
+		
+		System.out.println("good ï¼Œi am your friend !");
 
 
 	}
